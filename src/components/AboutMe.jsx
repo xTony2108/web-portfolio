efd83be1df0a7fe486f432c3dd66c1960ecd4ebb
Antonino Alampi <Alampiantonino99@gmail.com>
2023-12-21 22:23:37 +0100

    Creata sezione about me

---
+++ src/components/AboutMe.jsx
@@ -0,0 +1,29 @@
+export const AboutMe = () => {
+  return (
+    <div className="bg-[#101118] py-sectionPY">
+      <h2 className="text-6xl text-white text-center font-black">Chi sono</h2>
+      <div className="max-w-sectionWidth m-auto pt-6 flex flex-col items-center">
+        <p className="text-[#F4E2C9] text-xl font-bold pb-8">
+          Mi chiamo Antonino Alampi sono nato e cresciuto a Reggio Calabria il
+          21/08/1999. Sin da piccolo ho avuto una forte passione per
+          l'informatica osservando i miei fratelli utilizzare computer e
+          console. Nel 2013 ho frequentato l'istituto tecnico tecnologico "ITT
+          Panella-vallauri" conseguendo nel 2018 il diploma come perito
+          informatico con una valutazione di 83/100. Terminata la scuola ho
+          fatto vari lavoretti fino al 2020 quando mi trasferì in provincia di
+          Milano per lavorare presso Poste Italiane come addetto allo
+          smistamento. Terminato il contratto tornai alla mia città natale dove
+          trovai un lavoro come operatore di call center inbound presso System
+          House SRL. Infine nel 2023 conobbi Develhope che, attraverso
+          videolezioni, tutor e progetti pratici in team, mi ha permesso dopo 6
+          mesi di imparare le basi della programmazione Full Stack così da
+          aiutarmi trasformare le mie passioni in lavoro.
+        </p>
+        <img
+          src="https://api.accredible.com/v1/frontend/credential_website_embed_image/certificate/82106770"
+          alt="Attestato di Fullstack developer fornito da Develhope"
+        />
+      </div>
+    </div>
+  );
+};
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import { Navbar } from "../components/Navbar";
 import { Hero } from "../components/Hero";
+import { AboutMe } from "../components/AboutMe";
 
 export const Home = () => {
   return (
     <>
       <Navbar />
       <Hero />
+      <AboutMe />
     </>
   );
 };
