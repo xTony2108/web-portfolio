e29204438e0591dd3af4c468568869369529df06
Antonino Alampi <Alampiantonino99@gmail.com>
2023-12-22 18:30:47 +0100

    Creati carosello e sezione progetti

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -25,7 +25,7 @@ export const Hero = () => {
 
   return (
     <>
-      <header>
+      <header className="relative">
         <div className="flex justify-center min-h-[calc(100svh-58px)] py-heroPY max-w-sectionWidth m-auto">
           <div className="flex-1 w-full">
             <div className="w-full">
@@ -78,19 +78,19 @@ export const Hero = () => {
               />
             </div>
           </div>
-          <div class="custom-shape-divider-bottom-1703247122">
-            <svg
-              data-name="Layer 1"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 1200 120"
-              preserveAspectRatio="none"
-            >
-              <path
-                d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z"
-                class="shape-fill"
-              ></path>
-            </svg>
-          </div>
+        </div>
+        <div className="custom-shape-divider-bottom-1703247122">
+          <svg
+            data-name="Layer 1"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 1200 120"
+            preserveAspectRatio="none"
+          >
+            <path
+              d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z"
+              className="shape-fill"
+            ></path>
+          </svg>
         </div>
       </header>
     </>
